a4538ed4d0dbf665134c8ed111a8aea76de5da82
Gerry Gehrmann <gerry.gehrmann@googlemail.com>
2016-04-27 19:41:08 +0000

    * Add authenticated directive * More styling

---
+++ public/resources/js/directives/authenticated.js
@@ -0,0 +1,43 @@
+/* global angular */
+/*
+ * authenticated.js: GNU GENERAL PUBLIC LICENSE Version 3
+ */
+(function() {
+  "use strict";
+
+  /**
+   * Directive that hides an element if the current user is not authenticated.
+   * 
+   * @autor Gerry Gehrmann
+   * @since 0.0.1
+   */
+  angular.module("taskplanner").directive('authenticated', Authenticated);
+
+  Authenticated.$inject = ['AuthService'];
+  
+  /**
+   * @name Authenticated
+   * @class
+   * @property {Object} AuthService - The authentication service
+   */
+  function Authenticated(AuthService) {
+
+    return {
+      link : function(scope, element, attrs) {
+        var value = attrs.authenticated;
+        var parent = element.parent();
+
+        function toggleVisibilityBasedOnPermission() {
+          if (((!value || value === '' || value === 'true') && AuthService.getUser() != undefined) || (value === 'false' && AuthService.getUser() === undefined)) {
+            parent.append(element);
+          } else {
+            element.detach();
+          }
+        }
+
+        toggleVisibilityBasedOnPermission();
+        scope.$on('permissionsChanged', toggleVisibilityBasedOnPermission);
+      }
+    };
+  }
+})();
